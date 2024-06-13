7a95d9aa11ea50c44552e6d9e5f057b16c65cb66
Alexey Myth <flames.soad@gmail.com>
2024-06-13 22:50:20 +0700

    3

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print ('hello')
-a = '1'
-print (a)
-print (a)
+import sqlite3
+
+con = sqlite3.connect(new.db)
+cur = con.cursor()
+res = cur.execute("SELECT * FROM Muvies")
+films = res.fetchall()
+
+print (films)
